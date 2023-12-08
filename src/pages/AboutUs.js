e68d9677fa32401a32c67c7453f582e2a4f54d63
Antonov-S <anto1nov1@gmail.com>
2023-12-08 18:55:05 +0200

    Adding a ServicesSection

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 export default function AboutUs() {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
 }
