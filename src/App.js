e8ef3e6970091ba0047ad01eab05ea0714cca0ba
Antonov-S <anto1nov1@gmail.com>
2023-12-09 21:26:21 +0200

    Setting up React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
 
 import GlobalStyle from "./components/GlobalStyle";
+
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" exact element={<AboutUs />} />
+        <Route path="/work" exact element={<OurWork />} />
+        <Route path="/contact" exact element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
