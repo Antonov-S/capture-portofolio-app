65c302c78e14b440ff16d6a6b7f10963a03f40d5
Antonov-S <anto1nov1@gmail.com>
2023-12-10 13:49:21 +0200

    Adding html and some styles for Our Work

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,9 +1,61 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
 
 export default function OurWork() {
   return (
-    <div>
-      <h1>OurWork</h1>
-    </div>
+    <Work>
+      <Movie>
+        <h2>The Athlete</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt="Athlete" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt="Racer" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt="Good times" />
+        </Link>
+      </Movie>
+    </Work>
   );
 }
+
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  background: white;
+
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+
+const Movie = styled.div`
+  padding-bottom: 10rem;
+
+  .line {
+    height: 0.5rem;
+    background: #23d997;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
