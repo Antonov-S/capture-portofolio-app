d7a1dc9c1938f4766702394f9d2c644a2d45c9bd
Antonov-S <anto1nov1@gmail.com>
2023-12-15 23:52:47 +0200

    Adding About Animations

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 
 import home1 from "../img/home1.png";
 import { About, Description, Image, Hide } from "../styles";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 export default function AboutSection() {
   return (
@@ -10,22 +11,22 @@ export default function AboutSection() {
       <Description>
         <motion.div className="title">
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come{" "}
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
-        <button>Contact Us</button>
+        <motion.p variants={fade}>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="Guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="Guy with a camera" />
       </Image>
     </About>
   );
